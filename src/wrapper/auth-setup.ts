bc079d4630e8e7083c95a5f6b2f4360ab443c0ee
evgeny <ekarpovsky@gmail.com>
2023-11-22 05:47:40 +0000

    Clean code before publishing

--- src/wrapper/auth-setup.ts
+++ src/wrapper/auth-setup.ts
@@ -2,14 +2,11 @@ import { AuthMngrOPtions, setupAuthManager } from '@juliusagency/base-user-mngr'
 import { initStrategies, StrategyOptions } from '@juliusagency/auth-strategies';
 import { BaseUser, dBApi, Token } from '@juliusagency/base-user-mongo';
 import { AuthJwtOptions, setupAuthMiddleware } from '@juliusagency/auth-jwt';
-// import { TransportConfig } from '@juliusagency/simple-email-client';
 
 // Reexport from the packages
 export { BaseUser, AuthJwtOptions };
 
 export type AuthJwtSetSetupOptions = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // repository: any;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   User: any;
   authOpt: AuthJwtOptions;
@@ -29,12 +26,9 @@ export const authSetSetup = (config: AuthJwtSetSetupOptions) => {
 
   const strategy = initStrategies(strategyOptions);
 
-  // const emailer = new EmailClient(configApp.transport);
-
   config.User = user;
   // Auth middleware setup
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // const authMiddleware = setupAuthMiddleware(config.authOpt);
   const { authMiddleware, encodeToken } = setupAuthMiddleware(config.authOpt);
 
   const authMngrOPtions: AuthMngrOPtions = {
